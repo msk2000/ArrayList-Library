690f9cb09a23115cc23c27f1e5747ce83ee86071
Safat <your_username@users.noreply.github.com>
2024-09-10 11:20:57 -0400

    added the remove element at index function

--- Array_List_Library.cpp
+++ Array_List_Library.cpp
@@ -3,7 +3,8 @@
 
 #define DEFAULT_SIZE_OF_LIST 10
 
-class ArrayList {
+class ArrayList 
+{
 
     private:
     int* data;
@@ -13,31 +14,36 @@ class ArrayList {
     public:
 
     //Constructor
-    ArrayList(): listLength(0), arraySize(DEFAULT_SIZE_OF_LIST){
+    ArrayList(): listLength(0), arraySize(DEFAULT_SIZE_OF_LIST)
+    {
 
         data = (int*)malloc(arraySize* sizeof(int));
         //handling memory allocation failure
-        if (!data) {
+        if (!data) 
+        {
         std::cerr << "Failed to allocate memory for ArrayList" << std::endl;
         std::exit(EXIT_FAILURE);
-    }
+        }
     }
     // Destructor
-    ~ArrayList(){
+    ~ArrayList()
+    {
 
         free(data);
 
     }
 
     // Function to insert an element into the list
-    int InsertElement(int value, int index){
+    int InsertElement(int value, int index)
+    {
 
         //This is for sanity (no negative index!)
         std::cerr << "Index out of range!" << std::endl;
         return -9999; 
 
         //Safety: We can't allow list to get bigger than heap allocation. So we reallocate memory
-        if(listLength+1 > arraySize){
+        if(listLength+1 > arraySize)
+        {
             std::cerr <<"We need larger space! Current array located at "<< (void*)data << ", size: "<< arraySize <<std::endl;
             arraySize++;
             data = (int*)realloc(data,arraySize*sizeof(int));
@@ -45,9 +51,11 @@ class ArrayList {
         }
 
         //If the numebr of meaningful elements in the list is larger than the target index, insertion is possible:
-        if(listLength >= index){
+        if(listLength >= index)
+        {
             //First insert the element in that spot: Start by moving every item by one spot to create space for the new one
-            for(int i = listLength; i > index; i--){
+            for(int i = listLength; i > index; i--)
+            {
                 data[i] = data[i-1];
             }
             //Insert the value to the target index
@@ -57,7 +65,8 @@ class ArrayList {
         }
 
         // If list ends up being smaller than the target index, just add the value to the last spot 
-        else{
+        else
+        {
             // Insert item to location listLength + 1 (because index is larger than the listLength here)
             index = listLength;
             listLength++;
@@ -67,7 +76,46 @@ class ArrayList {
         // We can confirm the insertion point by returning the index
 
         return index;
+    }
 
+    // Function to Remove Elements from the list
+    int RemoveElementAtIndex(int index)
+    {
+
+        int removalTarget;
+
+        //Sanity Check - index value has to make sense
+        if(index >= listLength || index < 0)
+        {
+            std::cerr<<"Index out of Range!"<<std::endl;
+            return -9999; 
+        }
+
+        removalTarget = data[index];
+
+        // Remove the element at the given location
+        // First we move everything by 1 step and overwrite the value to be removed
+        for(int i = index; i < listLength; i++)
+        {
+            data[i] = data[i+1];
+        }
+
+        listLength--; // decrease the list size by 1
+
+        // Decrease the primitive array size by 1 (to save memory)
+        if(arraySize > DEFAULT_SIZE_OF_LIST)
+        {
+            std::cout<<"Need to Downsize! Current array located at "<<(void*)data<<", and size: "<<arraySize<<std::endl;
+            arraySize--;
+            data = (int*)realloc(data, arraySize * sizeof(int));
+            std::cout<<"Downsize Completed! New array located at "<<(void*)data<<", and size: "<<arraySize<<std::endl;
+        } 
+        // return the value removed from the list- for future usecase 
+        return removalTarget;
+
+
+
+    }
 
 
 
